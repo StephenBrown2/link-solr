ff8fae96d69d2ed0849731c52e707b5773dbf3bd
Stephen Brown II <stbrown@jhuccp.org>
2012-11-30 13:44:03 -0500

    Initial commit

---
+++ link-solr.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This is the path to the configuration from the apachesolr module,
+# e.g. '/var/www/d7_sites4dev/htdocs/sites/all/modules/contrib/apachesolr/solr-conf/solr-3.x'
+# FROM:
+confpath=$1
+
+if [ $confpath == '.' ]; then
+    confpath=$(pwd);
+fi
+
+# This is the path where the solr installation lies, the actual core configuration
+# TO:
+solrpath=$2
+
+# Neither path should have a trailing slash
+
+timestamp=$(date +%s)
+
+for file in `ls --color=never $confpath`; do
+  echo
+  echo "Backing up $solrpath/$file"
+  mv $solrpath/$file{,.$timestamp.bak}
+  echo "Linking $file"
+  ln -s $confpath/$file $solrpath/
+done
+
+echo
+echo "Please verify:"
+echo ls -Al $solrpath
+ls -Al --color $solrpath
