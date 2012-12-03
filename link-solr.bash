d67492599f28e9013259c658a32cbd931e902a9b
Stephen Brown II <stbrown@jhuccp.org>
2012-12-03 15:02:58 -0500

    Overhaul to use options to clarify source and destination paths

--- link-solr.bash
+++ link-solr.bash
@@ -1,31 +1,50 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Required arguments: 2, Entered arguments: $#"
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 --from <confpath> --to <solrpath>"
     echo "Please enter the path to the configuration from the apachesolr module,"
     echo " followed by the path where the solr installation lies, i.e. the actual core-name/conf."
     echo "Neither path should have a trailing slash."
     exit
 fi
 
-# This is the path to the configuration from the apachesolr module,
-# e.g. '/var/www/d7_sites4dev/htdocs/sites/all/modules/contrib/apachesolr/solr-conf/solr-3.x'
-# FROM:
-confpath=${1%/}
+confpath='false';
+solrpath='false';
+
+OPTS=`getopt -o s:d: -l src:,dst:,from:,to: -- "$@"`
+
+eval set -- "$OPTS"
+
+while true ; do
+    case "$1" in
+        # This is the path to the configuration from the apachesolr module,
+        # e.g. '/var/www/d7_sites4dev/htdocs/sites/all/modules/contrib/apachesolr/solr-conf/solr-3.x'
+        # FROM:
+        -s | --src  | --from ) confpath="${2%/}"; shift; shift;;
+
+        # This is the path where the solr installation lies, the actual core configuration
+        # e.g. '/usr/local/solr/drupal/multicore/d7-bhe/conf'
+        # TO:
+        -d | --dst  | --to   ) solrpath="${2%/}"; shift; shift;;
+
+        # Neither path should have a trailing slash
+
+        --) shift; break;;
+        * ) break ;;
+    esac
+done
 
 if [ $confpath == '.' ]; then
     confpath=$(pwd);
 fi
 
-# This is the path where the solr installation lies, the actual core configuration
-# e.g. '/usr/local/solr/drupal/multicore/d7-bhe/conf'
-# TO:
-solrpath=${2%/}
+if [ $solrpath == '.' ]; then
+    solrpath=$(pwd);
+fi
 
-# Neither path should have a trailing slash
-
-if [ ! -d $confpath -o ! -d $solrpath ]; then
-    echo "Please enter a valid file path for both arguments."
+if [ ! -d $confpath -o "$confpath" == "false" -o ! -d $solrpath -o "$solrpath" == "false" ]; then
+    echo "Usage: $0 --from <confpath> --to <solrpath>"
+    echo "Please enter a valid directory path for both arguments."
     echo "Neither path should have a trailing slash."
     exit
 fi
